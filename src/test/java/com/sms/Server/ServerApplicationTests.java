2076857e887ef9533e063132d45721f6a2db2c14
Chemistry2i <wambogohassan63@gmail.com>
2025-11-17 16:31:27 +0300

    Initial commit

---
+++ src/test/java/com/sms/Server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sms.Server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
